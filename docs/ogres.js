60ab5d68fd63fe07de07e6eb72aa97b376d084a5
A Faraday (post baby) <ajfaraday@edge-ka.com>
2021-07-22 20:39:02 +0100

    Automated deploy: 2021-07-22_20:39:02

--- docs/ogres.js
+++ docs/ogres.js
@@ -1,1 +1,1 @@
-ogres =  [{"name":"default/half_way","hand":2,"np":125,"reason":"Winner!"},{"name":"default/go_big","hand":1,"np":5,"reason":"Cowardice!"},{"name":"default/go_home","hand":0,"np":499,"reason":"Cowardice!"}]
+ogres =  [{"name":"default/half_way","hand":3,"np":63,"reason":"Winner!"},{"name":"default/go_big","hand":2,"np":1,"reason":"Cowardice!"},{"name":"imported/redwolf_programs/_dumb__ogre","hand":1,"np":496,"reason":"Cowardice!"},{"name":"default/go_home","hand":0,"np":499,"reason":"Cowardice!"}]
