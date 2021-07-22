246092b2c20efe82da72d569071d50c28c3f4de9
A Faraday (post baby) <ajfaraday@edge-ka.com>
2021-07-22 14:14:13 +0100

    Removed test answer

--- script/fetch_answers.js
+++ script/fetch_answers.js
@@ -1,6 +1,6 @@
 const AnswerFetcher = require('./../src/import/answer_fetcher.js')
 
-var fetcher = new AnswerFetcher('codegolf.meta', 23701);
+var fetcher = new AnswerFetcher('codegolf', 231681);
 fetcher.clear_directory();
 fetcher.get_answers();
 
