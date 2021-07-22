ca76dc49fd809683e0fc8164d1d1d5a5c30e66e6
A Faraday (post baby) <ajfaraday@edge-ka.com>
2021-07-22 13:02:40 +0100

    Nevermind, it's docs now

--- grab_and_deploy.sh
+++ grab_and_deploy.sh
@@ -6,10 +6,10 @@ echo "Fetching and importing answers"
 node script/fetch_answers.js
 node script/get_clients.js
 echo "Running tournament"
-node script/play_game.js > public/report.txt
+node script/play_game.js > docs/report.txt
 echo "Deploying to git"
 git add src/clients/*
 now=`date '+%F_%H:%M:%S'`;
 commit_message="Automated deploy: ${now}"
-git commit src/clients/* public/*
+git commit src/clients/* docs/*
 git push
