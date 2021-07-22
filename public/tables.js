4de4287c5b549cd1dacbdb2fe34e9ef80d7b97f1
A Faraday (post baby) <ajfaraday@edge-ka.com>
2021-07-22 12:57:07 +0100

    Actual results!

---
+++ public/tables.js
@@ -0,0 +1,1 @@
+ogres =  [{"name":"default/half_way","hand":3,"np":63,"reason":"Winner!"},{"name":"default/go_big","hand":2,"np":1,"reason":"Cowardice!"},{"name":"imported/ajfaraday/test_answer","hand":1,"np":470,"reason":"Cowardice!"},{"name":"default/go_home","hand":0,"np":499,"reason":"Cowardice!"}]
