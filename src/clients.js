c45756ba635f54314519ea15b19735fcba766b80
A Faraday (post baby) <ajfaraday@edge-ka.com>
2021-07-22 14:14:13 +0100

    Removed test answer

--- src/clients.js
+++ src/clients.js
@@ -2,5 +2,4 @@ module.exports = {
   'default/go_big': require('./clients/default/go_big.js'),
   'default/go_home': require('./clients/default/go_home.js'),
   'default/half_way': require('./clients/default/half_way.js'),
-  'imported/ajfaraday/test_answer': require('./clients/imported/ajfaraday/test_answer.js'),
 };
