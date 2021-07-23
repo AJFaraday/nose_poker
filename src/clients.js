b9068b468e87450c80df11d9e2350f2120ef4a56
A Faraday (post baby) <ajfaraday@edge-ka.com>
2021-07-23 16:01:33 +0100

    Automated deploy: 2021-07-23_16:01:33

--- src/clients.js
+++ src/clients.js
@@ -6,6 +6,7 @@ module.exports = {
   'imported/mousetail/_troll__adaptive': require('./clients/imported/mousetail/_troll__adaptive.js'),
   'imported/nurator/_adaptive__bot': require('./clients/imported/nurator/_adaptive__bot.js'),
   'imported/redwolf_programs/_dumb__ogre': require('./clients/imported/redwolf_programs/_dumb__ogre.js'),
+  'imported/redwolf_programs/_reverse__pinocchio': require('./clients/imported/redwolf_programs/_reverse__pinocchio.js'),
   'imported/redwolf_programs/_sinus-oidal': require('./clients/imported/redwolf_programs/_sinus-oidal.js'),
   'imported/user/_half-life': require('./clients/imported/user/_half-life.js'),
 };
