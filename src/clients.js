ee7522841829bdf8a54a272c4d7326defeb84f9e
A Faraday (post baby) <ajfaraday@edge-ka.com>
2021-07-23 10:52:10 +0100

    Automated deploy: 2021-07-23_10:52:10

--- src/clients.js
+++ src/clients.js
@@ -2,7 +2,4 @@ module.exports = {
   'default/go_big': require('./clients/default/go_big.js'),
   'default/go_home': require('./clients/default/go_home.js'),
   'default/half_way': require('./clients/default/half_way.js'),
-  'imported/redwolf_programs/_dumb__ogre': require('./clients/imported/redwolf_programs/_dumb__ogre.js'),
-  'imported/redwolf_programs/_sinus-oidal': require('./clients/imported/redwolf_programs/_sinus-oidal.js'),
-  'imported/user/_half-life': require('./clients/imported/user/_half-life.js'),
 };
